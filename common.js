12cf20af1ea40d16c9aeabdb0ecccb79328fac5a
junxxzz <69663933+junxxzz@users.noreply.github.com>
2024-01-05 11:36:48 +0900

    aaa

---
+++ common.js
@@ -0,0 +1,38 @@
+window.alert = function(){};
+window.addEventListener("load", (event) => {
+	renderingIncludePath();
+});
+
+function renderingIncludePath() {
+	document.querySelectorAll("[data-include-path]").forEach((el) => {
+		fetch(el.dataset.includePath).then((d) => {
+			if( !d.ok ) return;
+			d.text().then((t) => {
+				el.outerHTML = t;
+				if( window.renderingComplete ) {
+					window.renderingComplete(el.dataset.includePath);
+				}
+			});
+		});
+	});
+	setTimeout(function() {
+		if( document.querySelectorAll("[data-include-path]").length ) {
+			renderingIncludePath();
+		}
+	}, 100);
+}
+function renderingComplete(includePath) {
+	if( includePath=='footer.htm' ) {
+        document.querySelector('footer').append('<dialog id="alert"><div><fieldset id="alertMessage"></fieldset><input type="button" id="alertCloseButton" value="CLOSE" onclick="closeAlert()"></div></dialog>');
+        // <dialog id="circle"><div></div></dialog>
+
+		window.alert = function(arg) {
+			if( arguments.length==2 && (typeof arguments[1])=='function' ) {
+				window.closeAlertFunction = arguments[1];
+			}
+			const al = document.querySelector('#alert');
+			al.querySelector('fieldset#alertMessage').innerHTML = arguments[0];
+			al.showModal();
+		}
+	}
+}
