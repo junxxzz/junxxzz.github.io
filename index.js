dc79da8a187eb25a2dae4e6cd1f9ae5048ac8f43
junxxzz <69663933+junxxzz@users.noreply.github.com>
2024-01-08 16:42:31 +0900

    add oauth test

--- index.js
+++ index.js
@@ -1,75 +1,127 @@
-const {markedHighlight} = globalThis.markedHighlight;
+const { markedHighlight } = globalThis.markedHighlight;
 marked.use(markedDirective.createDirectives());
 // marked.use(markedLinkifyIt({},{}));
-marked.use(markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang, info) {
-        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-        const addon = `<div class='language'>${language}</div>`;
-        return hljs.highlight(code, { language }).value + addon;
-    }
-}));
-marked.use({ breaks: true, });
+marked.use(
+	markedHighlight({
+		langPrefix: "hljs language-",
+		highlight(code, lang, info) {
+			const language = hljs.getLanguage(lang) ? lang : "plaintext";
+			const addon = `<div class='language'>${language}</div>`;
+			return hljs.highlight(code, { language }).value + addon;
+		},
+	}),
+);
+marked.use({ breaks: true });
 
-setLoadComplete(function() {
-    listenHash(loadArticle);
-    const articleTemplate = document.querySelector('template#articleTemplate').innerHTML;
-    const articleSection = document.querySelector('section#articles');
-    let maArticleIdx = 0;
-    fetch('/articles.dat').then(res => {
-        if( res.ok ) {
-            res.text().then(d => {
-                d.split('\n').reverse().forEach(line => {
-                    if( !line.trim() ) {
-                        return;
-                    }
-                    const [idx, writeAt, categorys, title] = line.split('|');
-                    if( maArticleIdx==0 ) {
-                        maArticleIdx = idx;
-                    }
-                    const newArticle = document.createElement('article');
-                    newArticle.setAttribute('id', `article-${idx}`);
-                    newArticle.innerHTML = articleTemplate.replace('{article-writeAt}',writeAt).replace('{article-title}',title);
-                    categorys.split(',').forEach(c => {
-                        const newIcon = document.createElement('img');
-                        newIcon.setAttribute('src',`/icons/${c.trim()}.svg`);
-                        newIcon.classList.add('articleIcon');
-                        newArticle.innerHTML = newArticle.innerHTML.replace('{article-icons}',newIcon.outerHTML);
-                    });
-                    newArticle.addEventListener('click', () => {
-                        setHash('articleId',idx);
-                    });
-                    articleSection.append(newArticle);
-                });
+setLoadComplete(function () {
 
-                if( !window.hash.articleId ) {
-                    window.hash.articleId = maArticleIdx;
-                    loadArticle(window.hash);
-                }
-            });
-        }
-    });
+    document.querySelector('#userlogin').addEventListener('click', oauthSignIn);
+
+	listenHash(loadArticle);
+
+	const articleTemplate = document.querySelector(
+		"template#articleTemplate",
+	).innerHTML;
+	const articleSection = document.querySelector("section#articles");
+	let maArticleIdx = 0;
+	fetch("/articles.dat").then((res) => {
+		if (res.ok) {
+			res.text().then((d) => {
+				d.split("\n")
+					.reverse()
+					.forEach((line) => {
+						if (!line.trim()) {
+							return;
+						}
+						const [idx, writeAt, categorys, title] = line.split("|");
+						if (maArticleIdx == 0) {
+							maArticleIdx = idx;
+						}
+						const newArticle = document.createElement("article");
+						newArticle.setAttribute("id", `article-${idx}`);
+						newArticle.innerHTML = articleTemplate
+							.replace("{article-writeAt}", writeAt)
+							.replace("{article-title}", title);
+						categorys.split(",").forEach((c) => {
+							const newIcon = document.createElement("img");
+							newIcon.setAttribute("src", `/icons/${c.trim()}.svg`);
+							newIcon.classList.add("articleIcon");
+							newArticle.innerHTML = newArticle.innerHTML.replace(
+								"{article-icons}",
+								newIcon.outerHTML,
+							);
+						});
+						newArticle.addEventListener("click", () => {
+							setHash("articleId", idx);
+						});
+						articleSection.append(newArticle);
+					});
+
+				if (!window.hash.articleId) {
+					window.hash.articleId = maArticleIdx;
+					loadArticle(window.hash);
+				}
+			});
+		}
+	});
 });
 
 function loadArticle(hash) {
-    if( hash.articleId > 0 ) {
-        fetch(`/articles/article_${hash.articleId}.md`).then(res => {
-            if( res.ok ) {
-                res.text().then(d => {
-                    document.getElementById('contents').innerHTML = DOMPurify.sanitize(marked.parse(d));
-                    window.scrollTo(0,0);
-                    gtag('event', 'article_view', {
-                        'articleId': hash.articleId,
-                        'result': 'success',
-                    });
-                });
-            }
-            else {
-                gtag('event', 'article_view', {
-                    'articleId': hash.articleId,
-                    'result': 'fail',
-                });
-            }
-        });
+	if (hash.articleId > 0) {
+		fetch(`/articles/article_${hash.articleId}.md`).then((res) => {
+			if (res.ok) {
+				res.text().then((d) => {
+					document.getElementById("contents").innerHTML = DOMPurify.sanitize(
+						marked.parse(d),
+					);
+					window.scrollTo(0, 0);
+					gtag("event", "article_view", {
+						articleId: hash.articleId,
+						result: "success",
+					});
+				});
+			} else {
+				gtag("event", "article_view", {
+					articleId: hash.articleId,
+					result: "fail",
+				});
+			}
+		});
+	}
+}
+
+/**
+ * Create form to request access token from Google's OAuth 2.0 server.
+**/
+function oauthSignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement("form");
+    form.setAttribute("method", "GET"); // Send as a GET request.
+    form.setAttribute("action", oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+        client_id: "920653369919-738ci7p79n38kvc9lv25ndfdvijm1kao.apps.googleusercontent.com",
+        redirect_uri: "https://junxxzz.github.io",
+        response_type: "token",
+        scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+        include_granted_scopes: "true",
+        state: "pass-through value",
+    };
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", p);
+        input.setAttribute("value", params[p]);
+        form.appendChild(input);
     }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
 }
